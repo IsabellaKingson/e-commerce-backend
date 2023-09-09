34662162d08c5a6fc6baadeeb103f83101b1414f
IsabellaKingson <isabellasophiaking@gmail.com>
2023-09-09 03:01:53 -0500

    First glance of te content getting as much work as I could without reference

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,32 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes,INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      // Validate that the value is a decimal
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      // Set a default value of 10
+      // Validate that the value is numeric
+      // References the category model's id
+    }
   },
   {
     sequelize,
